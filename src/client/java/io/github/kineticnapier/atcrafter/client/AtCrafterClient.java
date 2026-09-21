cb46cede2c48d43f22f416cbe2a5f72960f12587
Kinetic Napier <u61819269@gmail.com>
2026-09-22 00:01:40 +0900

    Poll runner status from client tick

--- src/client/java/io/github/kineticnapier/atcrafter/client/AtCrafterClient.java
+++ src/client/java/io/github/kineticnapier/atcrafter/client/AtCrafterClient.java
@@ -1,8 +1,10 @@
 package io.github.kineticnapier.atcrafter.client;
 
 import io.github.kineticnapier.atcrafter.block.ModBlocks;
+import io.github.kineticnapier.atcrafter.client.runner.RunnerClient;
 import io.github.kineticnapier.atcrafter.client.screen.TerminalScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.InteractionResult;
@@ -10,6 +12,8 @@ import net.minecraft.world.InteractionResult;
 public class AtCrafterClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> RunnerClient.tick());
+
         UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
             if (!world.isClientSide()) {
                 return InteractionResult.PASS;
@@ -17,7 +21,10 @@ public class AtCrafterClient implements ClientModInitializer {
 
             if (world.getBlockState(hitResult.getBlockPos()).is(ModBlocks.TERMINAL)) {
                 Minecraft minecraft = Minecraft.getInstance();
-                minecraft.execute(() -> minecraft.setScreen(new TerminalScreen()));
+                minecraft.execute(() -> {
+                    RunnerClient.checkNow();
+                    minecraft.setScreen(new TerminalScreen());
+                });
                 return InteractionResult.SUCCESS;
             }
 
