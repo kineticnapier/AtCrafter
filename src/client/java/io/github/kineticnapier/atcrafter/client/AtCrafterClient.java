3874a126eeb66451fa67d3cc59b966bafa32f0cf
Kinetic Napier <kiner5674@gmail.com>
2026-09-21 23:41:28 +0900

    init

---
+++ src/client/java/io/github/kineticnapier/atcrafter/client/AtCrafterClient.java
@@ -0,0 +1,10 @@
+package io.github.kineticnapier.atcrafter.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AtCrafterClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
