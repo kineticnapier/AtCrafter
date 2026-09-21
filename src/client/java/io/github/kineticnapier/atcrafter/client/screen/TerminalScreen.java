6dee84d63ee3bcf764f830e0eb6e2fab9ac28505
Kinetic Napier <u61819269@gmail.com>
2026-09-21 23:54:55 +0900

    Add initial terminal screen

---
+++ src/client/java/io/github/kineticnapier/atcrafter/client/screen/TerminalScreen.java
@@ -0,0 +1,51 @@
+package io.github.kineticnapier.atcrafter.client.screen;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public final class TerminalScreen extends Screen {
+    private static final Component TITLE = Component.literal("AtCrafter");
+
+    public TerminalScreen() {
+        super(TITLE);
+    }
+
+    @Override
+    protected void init() {
+        int buttonWidth = 120;
+        int buttonHeight = 20;
+        int x = (this.width - buttonWidth) / 2;
+        int y = this.height / 2 + 35;
+
+        addRenderableWidget(
+            Button.builder(Component.literal("Close"), button -> onClose())
+                .bounds(x, y, buttonWidth, buttonHeight)
+                .build()
+        );
+    }
+
+    @Override
+    public void render(GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
+        renderBackground(graphics, mouseX, mouseY, partialTick);
+        super.render(graphics, mouseX, mouseY, partialTick);
+
+        int centerX = this.width / 2;
+        int top = this.height / 2 - 55;
+
+        graphics.drawCenteredString(this.font, TITLE, centerX, top, 0xFFFFFF);
+        graphics.drawCenteredString(
+            this.font,
+            Component.literal("Runner: Offline"),
+            centerX,
+            top + 28,
+            0xA0A0A0
+        );
+    }
+
+    @Override
+    public boolean isPauseScreen() {
+        return false;
+    }
+}
