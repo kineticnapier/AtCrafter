12906483496b20f32e68a09ed469d048aef96339
Kinetic Napier <u61819269@gmail.com>
2026-09-21 23:55:02 +0900

    Open terminal screen on right click

--- src/client/java/io/github/kineticnapier/atcrafter/client/AtCrafterClient.java
+++ src/client/java/io/github/kineticnapier/atcrafter/client/AtCrafterClient.java
@@ -1,10 +1,22 @@
 package io.github.kineticnapier.atcrafter.client;
 
+import io.github.kineticnapier.atcrafter.block.ModBlocks;
+import io.github.kineticnapier.atcrafter.client.screen.TerminalScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.InteractionResult;
 
 public class AtCrafterClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    @Override
+    public void onInitializeClient() {
+        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+            if (world.getBlockState(hitResult.getBlockPos()).is(ModBlocks.TERMINAL)) {
+                Minecraft.getInstance().setScreen(new TerminalScreen());
+                return InteractionResult.SUCCESS;
+            }
+
+            return InteractionResult.PASS;
+        });
+    }
 }
